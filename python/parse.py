9140f529e9a59d9eb90a4552c79456a0062d146d
PravTadikonda <pravtadikonda410@yahoo.com>
2016-02-20 03:47:48 -0500

    Fixed parsing

--- python/parse.py
+++ python/parse.py
@@ -1,25 +1,25 @@
 from sys import argv
 from collections import namedtuple
 import fileinput
+import re
+import datetime
 
-ComStruct = namedtuple("StartDate", "Type", "Package", "Dependencies")
+ComStruct = namedtuple("ComStruct", "StartDate Type Packages")
+
+def command_handle(command):
+    if ("Upgrade: " in command):
+        return None
+
+    fields = command.split("\n")
+    
+    time = datetime.datetime.strptime(fields[0].split(": ")[1], "%Y-%m-%d %H:%M:%S")
+    commandType = fields[2].split(": ")[0]
+    packages = re.sub(r'\([^)]*\)', '', fields[2].split(": ")[1]).split(", ")
+
+    return ComStruct(StartDate = time, Type = commandType, Packages = packages)
 
 
 
-logfile = open(argv[1], 'r')  #accepts the 2nd parameter as the input for parsing. 
+logfile = open(argv[1], 'r')  #accepts the 2nd parameter as the input for parsing.
 commands = logfile.read()[1:].split("\n\n")
 command_structs = map(command_handle, commands)
-
-    
-
-'''
-for line in logfile:
-    parts = line.split(':',1)
-'''
-    
-def command_handle(command):
-    
-    
-    
-
-
